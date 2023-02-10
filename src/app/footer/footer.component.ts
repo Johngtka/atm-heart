084e36d5690fadd40f5654fbc624c43b6707712b
Johngtka <johngtka@gmail.com>
2023-02-10 13:03:59 +0100

    fix: moved the translation buttons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core'
-import { TranslateService } from '@ngx-translate/core'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-  constructor(private translateService: TranslateService) {}
-
-  setLang(lang: string) {
-    this.translateService.use(lang)
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core'
-
+import { TranslateService } from '@ngx-translate/core'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private translateService: TranslateService) { }
+
+  setLang(lang: string) {
+    this.translateService.use(lang)
+  }
+}
