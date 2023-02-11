91214c7a2ed7c82e68cb852abced89f4a34afbb9
Johngtka <johngtka@gmail.com>
2023-02-11 17:30:37 +0100

    chore: formated

--- src/app/updates/updates.component.ts
+++ src/app/updates/updates.component.ts
@@ -5,36 +5,4 @@ import { Component } from '@angular/core'
   templateUrl: './updates.component.html',
   styleUrls: ['./updates.component.css'],
 })
-export class UpdatesComponent {
-  name!: HTMLElement
-  sec: string[]
-  con: string[]
-  constructor() {
-    ;(this.sec = [
-      'UPDATS.SECT.2',
-      'UPDATS.SECT.3',
-      'UPDATS.SECT.4',
-      'UPDATS.SECT.5',
-      'UPDATS.SECT.6',
-      'UPDATS.SECT.7',
-      'UPDATS.SECT.8',
-      'UPDATS.SECT.9',
-      'UPDATS.SECT.10',
-      'UPDATS.SECT.11',
-      'UPDATS.SECT.12',
-    ]),
-      (this.con = [
-        'UPDATS.CONTENT.B2',
-        'UPDATS.CONTENT.B3',
-        'UPDATS.CONTENT.B4',
-        'UPDATS.CONTENT.B5',
-        'UPDATS.CONTENT.B6',
-        'UPDATS.CONTENT.B7',
-        'UPDATS.CONTENT.B8',
-        'UPDATS.CONTENT.B9',
-        'UPDATS.CONTENT.B10',
-        'UPDATS.CONTENT.B11',
-        'UPDATS.CONTENT.B12',
-      ])
-  }
-}
+export class UpdatesComponent {}
