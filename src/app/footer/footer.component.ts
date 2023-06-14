51b4898bdd6d575afd7785f4ee11d3cca4614a0c
Johngtka <79154804+Johngtka@users.noreply.github.com>
2023-06-14 19:23:35 +0200

    feat: version tag showing (#11)

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 @Component({
     selector: 'atm-footer',
     templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+    version = environment.version;
+    title = environment.TITLE;
+}
