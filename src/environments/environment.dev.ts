d19d5f28cbd8451e9a9831d0c48ba9dab4f656a4
Johngtka <johngtka@gmail.com>
2023-06-14 19:14:28 +0200

    feat:added tag showing

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    TITLE: 'Atomic_DEV',
+    version: 'VERSION-DEV',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    TITLE: 'Atomic',
+    version: '0.0.0',
+};
