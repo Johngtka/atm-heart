bef61ba882240b3066ad151a32ef3e3736676fa9
Johngtka <johngtka@gmail.com>
2023-11-30 14:19:54 +0100

    feat: added material alert for app ending info with translations

--- src/app/atm.component.ts
+++ src/app/atm.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'atm-root',
     templateUrl: './atm.component.html',
     styleUrls: ['./atm.component.css'],
 })
-export class AtmComponent {}
+export class AtmComponent implements OnInit {
+    appEndingAlert!: boolean;
+    ngOnInit(): void {
+        this.appEndingAlert = true;
+    }
+    disableEndingAppAlert(): void {
+        this.appEndingAlert = false;
+    }
+}
