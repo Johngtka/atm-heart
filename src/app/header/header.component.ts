3e4ed512ba2985ca321ea27fdccb74768915b3cb
Johngtka <79154804+Johngtka@users.noreply.github.com>
2023-12-01 09:56:44 +0100

    feat: new view of app header component for mobile devices with code compress (#15)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
     selector: 'atm-header',
     templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+    isFooterMobile!: boolean;
+    constructor(private observer: BreakpointObserver) {}
+    ngOnInit(): void {
+        this.observer.observe(['(max-width: 600px)']).subscribe((device) => {
+            if (device.matches) {
+                this.isFooterMobile = true;
+            } else {
+                this.isFooterMobile = false;
+            }
+        });
+    }
+}
